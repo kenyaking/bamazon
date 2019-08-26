5ae3f5006ac632426a8f454e2dc87aab21e7efcd
Kenya <kenya55@msn.com>
2019-08-26 13:30:41 -0400

    created function for connecting table products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,5 +19,20 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  
+
+  showProducts();
 });
+
+function showProducts(){
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products"
+    connection.query(query, function(err,res){
+        var display = new table({
+            head: ["item_id", "product_name", "department_name", "price", "quantity"],
+            colWidths: [10, 30, 30, 10, 15]
+        })
+    for (var i=0; i<res.length; i++){
+        display.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+    }
+            console.log(display.toString())
+    });
+}
